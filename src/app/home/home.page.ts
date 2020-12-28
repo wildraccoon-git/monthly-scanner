820a44c8f6a2221fcd98e661af7eb4a7527e0a93
Aki <kenji_99072@yahoo.co.jp>
2020-12-29 08:39:19 +0900

    monthly-report

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedText: string;
+  format: string;
+  constructor(private barcodeScanner: BarcodeScanner) {}
+  scan(){
+    this.barcodeScanner.scan()
+    .then((barcodeData)=>{
+      if(barcodeData.cancelled){
+        this.setValues("", "(cancelled)");
+      }
+      else{
+        this.setValues(
+          barcodeData.text || '???',
+          barcodeData.format || '???'
+        );
+      }
+    })
+    .catch((err)=>{
+      this.setValues(`Error ${err}`, "");
+    });
 
-  constructor() {}
+  }
 
+  clear(){
+    this.setValues("", "");
+  }
+
+  setValues(s, f){
+    this.scannedText = s;
+    this.format = f;
+  }
 }
